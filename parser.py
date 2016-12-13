d61d0b91e1d5707bd7a7a4a40e754635abf98f08
Julien M'Poy <julien.mpoy@gmail.com>
2016-12-13 16:30:48 +0100

    add some grammar rules

--- parser.py
+++ parser.py
@@ -2,41 +2,25 @@ import ply.yacc as yacc
 
 from lex import tokens
 
-# operations = {
-#     '+': lambda x, y: x+y,
-#     '-': lambda x, y: x-y,
-#     '*': lambda x, y: x*y,
-#     '/': lambda x, y: x/y,
-# )
 
-
-def p_expression(p):
-    """ expression : expression_int
-        | expression_real """
+def p_file(p):
+    ''' file : program '''
     p[0] = p[1]
 
 
-def p_expression_int(p):
-    """ expression_int : INTEGER
-        | INTEGER ';' expression """
+def p_program(p):
+    ''' program : program_heading ';' block '.' '''
     p[0] = p[1]
 
 
-def p_expression_real(p):
-    """ expression_real : REAL
-        | REAL ';' expression """
-    p[0] = p[1]
+def p_program_heading(p):
+    ''' program_heading : PROGRAM IDENTIFIER ';' '''
+    p[0] = p[2]
 
 
 def p_error(p):
     print("Syntax error in line %d" % p.lineno)
     parser.errok()
-
-# precedence = (
-#     ('left', 'ADD_OP'),
-#     ('left', 'MUL_OP'),
-#     ('right', 'UMINUS'),
-# )
 
 
 parser = yacc.yacc(outputdir='generated')
