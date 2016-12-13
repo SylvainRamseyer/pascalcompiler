7afb4356f35523204788f7a4359bb28b2a1422b1
Julien M'Poy <julien.mpoy@gmail.com>
2016-12-13 15:46:20 +0100

    Parser with simple expressions support

--- parser.py
+++ parser.py
@@ -1,44 +1,48 @@
 import ply.yacc as yacc
-import AST
+
 from lex import tokens
 
-# variable dictionary
-vars = {}
+# operations = {
+#     '+': lambda x, y: x+y,
+#     '-': lambda x, y: x-y,
+#     '*': lambda x, y: x*y,
+#     '/': lambda x, y: x/y,
+# )
 
-# ERROR MANAGEMENT
+
+def p_expression(p):
+    """ expression : expression_int
+        | expression_real """
+    p[0] = p[1]
+
+
+def p_expression_int(p):
+    """ expression_int : INTEGER
+        | INTEGER ';' expression """
+    p[0] = p[1]
+
+
+def p_expression_real(p):
+    """ expression_real : REAL
+        | REAL ';' expression """
+    p[0] = p[1]
+
+
 def p_error(p):
-    if p:
-        print("Syntax error in line %d" % p.lineno)
-        yacc.errok()
-    else:
-        print("Sytax error: unexpected end of file!")
+    print("Syntax error in line %d" % p.lineno)
+    parser.errok()
+
+# precedence = (
+#     ('left', 'ADD_OP'),
+#     ('left', 'MUL_OP'),
+#     ('right', 'UMINUS'),
+# )
 
 
-# PARSING
-def parse(program):
-    return yacc.parse(program)
-
-# PRIORITY RULES
-precedence = (
-    ('left', 'ADD_OP'),
-    ('left', 'MUL_OP'),
-    ('right', 'UMINUS'),
-)
-
-yacc.yacc(outputdir='generated')
+parser = yacc.yacc(outputdir='generated')
 
 if __name__ == "__main__":
     import sys
-
     prog = open(sys.argv[1]).read()
-    result = yacc.parse(prog)
-    if result:
-        print(result)
-
-        import os
-        graph = result.makegraphicaltree()
-        name = os.path.splitext(sys.argv[1])[0]+'-ast.pdf'
-        graph.write_pdf(name)
-        print("wrote ast to", name)
-    else:
-        print("Parsing returned no result!")
+    result = yacc.parse(prog, debug=1)
+    print(result)
