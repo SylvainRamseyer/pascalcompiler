21407420dd53c0bd4ba16f308d2f9c73c8cd1569
Julien M'Poy <julien.mpoy@gmail.com>
2016-12-26 15:47:59 +0100

    AST file is on the way to respect PEP8

--- AST.py
+++ AST.py
@@ -1,23 +1,32 @@
-# coding: latin-1
-
-""" Petit module utilitaire pour la construction, la manipulation et la
-repr�sentation d'arbres syntaxiques abstraits.
-
-S�rement plein de bugs et autres surprises. � prendre comme un
-"work in progress"...
-Notamment, l'utilisation de pydot pour repr�senter un arbre syntaxique cousu
-est une utilisation un peu "limite" de graphviz. �a marche, mais le layout n'est
-pas toujours optimal...
 """
+AST module.
+
+Petit module utilitaire pour la construction, la manipulation et la
+représentation d'arbres syntaxiques abstraits.
+
+Sûrement plein de bugs et autres surprises. À prendre comme un
+"work in progress"...
+Notamment, l'utilisation de pydot pour représenter un arbre syntaxique cousu
+est une utilisation un peu "limite" de graphviz. ça marche, mais le layout
+n'est pas toujours optimal...
+"""
+
 import pydot
 
 
 class Node:
+    """Class representing an AST node."""
+
     count = 0
     type = 'Node (unspecified)'
     shape = 'ellipse'
 
     def __init__(self, children=None):
+        """
+        Constructor.
+
+        Children agrument is a list containg children of the current node.
+        """
         self.ID = str(Node.count)
         Node.count += 1
         if not children:
@@ -29,19 +38,24 @@ class Node:
         self.next = []
 
     def add_next(self, next):
+        """Adds a node to the current node."""
         self.next.append(next)
 
     def ascii_tree(self, prefix=''):
+        """Returns the ASCII tree for console displaying."""
         result = "%s%s\n" % (prefix, repr(self))
         prefix += '|  '
         for c in self.children:
             if not isinstance(c, Node):
-                result += "%s*** Error: Child of type %r: %r\n" % (prefix, type(c), c)
+                result += "%s*** Error: Child of type %r: %r\n" % (
+                    prefix, type(c), c
+                )
                 continue
             result += c.ascii_tree(prefix)
         return result
 
     def __str__(self):
+        """Returns the node's string reprensentation."""
         return self.ascii_tree()
 
     def __repr__(self):
@@ -50,7 +64,9 @@ class Node:
     def make_graphical_tree(self, dot=None, edge_labels=True):
             if not dot:
                 dot = pydot.Dot()
-            dot.add_node(pydot.Node(self.ID, label=repr(self), shape=self.shape))
+            dot.add_node(
+                pydot.Node(self.ID, label=repr(self), shape=self.shape)
+            )
             label = edge_labels and len(self.children) - 1
             for i, c in enumerate(self.children):
                 c.make_graphical_tree(dot, edge_labels)
@@ -59,9 +75,11 @@ class Node:
                     edge.set_label(str(i))
                 dot.add_edge(edge)
                 # Workaround for a bug in pydot 1.0.2 on Windows:
-                # dot.set_graphviz_executables({'dot': r'C:\Program Files\Graphviz2.16\bin\dot.exe'})
+                # dot.set_graphviz_executables({
+                #   'dot': r'C:\Program Files\Graphviz2.16\bin\dot.exe'
+                # })
             return dot
-        
+
     def thread_tree(self, graph, seen=None, col=0):
             colors = ('red', 'green', 'blue', 'yellow', 'magenta', 'cyan')
             if not seen:
@@ -71,7 +89,9 @@ class Node:
             seen.append(self)
             new = not graph.get_node(self.ID)
             if new:
-                graph_node = pydot.Node(self.ID, label=repr(self), shape=self.shape)
+                graph_node = pydot.Node(
+                    self.ID, label=repr(self), shape=self.shape
+                )
                 graph_node.set_style('dotted')
                 graph.add_node(graph_node)
             label = len(self.next)-1
@@ -84,12 +104,13 @@ class Node:
                 edge = pydot.Edge(self.ID, c.ID)
                 edge.set_color(color)
                 edge.set_arrowsize('.5')
-                # Les arr�tes correspondant aux coutures ne sont pas prises en compte
-                # pour le layout du graphe. Ceci permet de garder l'arbre dans sa repr�sentation
-                # "standard", mais peut provoquer des surprises pour le trajet parfois un peu
-                # tarabiscot� des coutures...
-                # En commantant cette ligne, le layout sera bien meilleur, mais l'arbre nettement
-                # moins reconnaissable.
+                # Les arrètes correspondant aux coutures ne sont pas prises en
+                # compte pour le layout du graphe. Ceci permet de garder
+                # l'arbre dans sa représentation "standard", mais peut
+                # provoquer des surprises pour le trajet parfois un peu
+                # tarabiscoté des coutures... En commantant cette ligne, le
+                # layout sera bien meilleur, mais l'arbre nettement moins
+                # reconnaissable.
                 edge.set_constraint('false')
                 if label:
                     edge.set_taillabel(str(i))
@@ -164,16 +185,16 @@ class EntryNode(Node):
 
 # DECORATOR
 def add_to_class(cls):
-    """ D�corateur permettant d'ajouter la fonction d�cor�e en tant que m�thode
-    � une classe.
+    """ Décorateur permettant d'ajouter la fonction décorée en tant que méthode
+    à une classe.
 
-    Permet d'impl�menter une forme �l�mentaire de programmation orient�e
-    aspects en regroupant les m�thodes de diff�rentes classes impl�mentant
-    une m�me fonctionnalit� en un seul endroit.
+    Permet d'implémenter une forme élémentaire de programmation orientée
+    aspects en regroupant les méthodes de différentes classes implémentant
+    une même fonctionnalité en un seul endroit.
 
-    Attention, apr�s utilisation de ce d�corateur, la fonction d�cor�e reste dans
-    le namespace courant. Si cela d�range, on peut utiliser del pour la d�truire.
-    Je ne sais pas s'il existe un moyen d'�viter ce ph�nom�ne.
+    Attention, après utilisation de ce décorateur, la fonction décorée reste
+    dans le namespace courant. Si cela dérange, on peut utiliser del pour la
+    détruire. Je ne sais pas s'il existe un moyen d'éviter ce phénomène.
     """
     def decorator(func):
         setattr(cls, func.__name__, func)
